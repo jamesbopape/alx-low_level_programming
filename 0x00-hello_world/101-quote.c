2e05b61cfd1172da67c73b260be9bcdc91bc7a8e
james bopape <jamesbopape@gmail.com>
2023-09-07 00:37:00 -0700

    hoh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void) {
+int main(void){
     fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
     return (1);
 }
