36ad143052add25f601eaa419f5dbb46b10dd97b
jamesbopape <jamesbopape@gmail.com>
2023-10-12 09:21:54 -0700

    sdfs

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,27 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * create_array - creates an array of chars, and
- * initializes it with a specific char.
- * @size: Size of the array
- * @c: Character to insert
- * Return: NULL if size is zero or if it fails,
- * pointer to array if everything is normal.
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
+	char *cr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size);
 
-	if (array == NULL)
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	for (i = 0; i < size; i++)
+		cr[i] = c;
 
-	return (array);
+	return (cr);
 }
